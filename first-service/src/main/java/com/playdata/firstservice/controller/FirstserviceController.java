962554eefb78b553cdf27c270bd71925f820b3e1
MEL1902 <1902otamel@gmail.com>
2023-10-16 17:02:17 +0900

    update20231016

--- first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
+++ first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
@@ -1,10 +1,15 @@
 package com.playdata.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("first-service") //gateway서버의 predicates의 식별 주소를 추가해야 합니다.
+@RequiredArgsConstructor
 public class FirstserviceController {
+
+    private final Environment env;
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello(){
@@ -16,4 +21,8 @@ public class FirstserviceController {
         return header;
     }
 
+    @GetMapping("port-check")
+    public String portCheck(){
+        return env.getProperty("local.server.port");
+    }
 }
--- second-service/src/main/java/com/playdata/secondservice/controller/SecondserviceController.java
+++ second-service/src/main/java/com/playdata/secondservice/controller/SecondserviceController.java
@@ -1,15 +1,25 @@
 package com.playdata.secondservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("second-service")
+@RequiredArgsConstructor
 public class SecondserviceController {
+
+    private final Environment env;
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello(){
-        return "Hello,Second-service!";
+        return "Hello,Second-service!"; }
+
+        @GetMapping("port-check")
+                public String portCheck(){
+            return env.getProperty("local.server.port");
+        }
     }
-}
