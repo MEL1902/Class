b3b26438c59c6414ecab0c9aac585563c357d190
MEL1902 <1902otamel@gmail.com>
2023-10-16 16:24:46 +0900

    update20231016

---
+++ dicovery-client/src/test/java/com/example/dicoveryclient/DicoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dicoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoveryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
